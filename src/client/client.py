d0680a9aa5e91a7cb880fb67fcbb6ba9bee706f6
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-07 22:18:45 +0700

    refactor: create class for server and client service

--- src/client/client.py
+++ src/client/client.py
@@ -11,117 +11,117 @@ import src.client.main_ui as ui2
 import src.client.registry_client as rc
 import src.client.shutdown_logout_client as sl
 
-# global variables
 BUFSIZ = 1024 * 4
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-root = tk.Tk()
-root.geometry("1000x600")
-root.configure(bg="#FFFFFF")
-root.title("Client")
-root.resizable(False, False)
-f1 = ui1.Entrance_UI(root)
 
 
-def back(ui):
-    ui.place_forget()
-    f2.place(x=0, y=0)
-    client.sendall(bytes("QUIT", "utf8"))
+class Client:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("1000x600")
+        self.root.configure(bg="#FFFFFF")
+        self.root.title("Client")
+        self.root.resizable(False, False)
+
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.f1 = ui1.Entrance_UI(self.root)
+        self.f1.button_1.configure(command=self.connect)
+
+    def start(self):
+        self.root.mainloop()
+
+    def back(self, ui):
+        ui.place_forget()
+        f2.place(x=0, y=0)
+        self.client.sendall(bytes("QUIT", "utf8"))
+
+    def live_screen(self):
+        self.client.sendall(bytes("LIVESCREEN", "utf8"))
+        tmp = lsc.Desktop_UI(self.root, self.client)
+        if not tmp.status:
+            self.back(tmp)
+        return
+
+    def shutdown_logout(self):
+        self.client.sendall(bytes("SD_LO", "utf8"))
+        sl.shutdown_logout(self.client, self.root)
+        return
+
+    def mac_address(self):
+        self.client.sendall(bytes("MAC", "utf8"))
+        mac.mac_address(self.client)
+        return
+
+    def back_dirTree(self, ui):
+        ui.place_forget()
+        ui.tree.pack_forget()
+        f2.place(x=0, y=0)
+        self.client.sendall(bytes("QUIT", "utf8"))
+
+    def directory_tree(self):
+        self.client.sendall(bytes("DIRECTORY", "utf8"))
+        tmp = dt.DirectoryTree_UI(self.root, self.client)
+        tmp.button_6.configure(command=lambda: self.back_dirTree(tmp))
+        return
+
+    def app_process(self):
+        self.client.sendall(bytes("APP_PRO", "utf8"))
+        tmp = ap.App_Process_UI(self.root, self.client)
+        tmp.button_6.configure(command=lambda: self.back(tmp))
+        return
+
+    def disconnect(self):
+        f2.place_forget()
+        self.f1.place(x=0, y=0)
+        self.client.sendall(bytes("QUIT", "utf8"))
+        return
+
+    def keylogger(self):
+        self.client.sendall(bytes("KEYLOG", "utf8"))
+        tmp = kl.Keylogger_UI(self.root, self.client)
+        tmp.button_6.configure(command=lambda: self.back(tmp))
+        return
+
+    def registry(self):
+        self.client.sendall(bytes("REGISTRY", "utf8"))
+        tmp = rc.Registry_UI(self.root, self.client)
+        tmp.btn_back.configure(command=lambda: self.back_reg(tmp))
+        return
+
+    def back_reg(self, ui):
+        ui.client.sendall(bytes("STOP_EDIT_REGISTRY", "utf8"))
+        ui.place_forget()
+        f2.place(x=0, y=0)
+
+    def show_main_ui(self):
+        self.f1.place_forget()
+        global f2
+        f2 = ui2.Main_UI(self.root)
+        f2.button_1.configure(command=self.live_screen)
+        f2.button_2.configure(command=self.registry)
+        f2.button_3.configure(command=self.mac_address)
+        f2.button_4.configure(command=self.directory_tree)
+        f2.button_5.configure(command=self.app_process)
+        f2.button_6.configure(command=self.disconnect)
+        f2.button_7.configure(command=self.keylogger)
+        f2.button_8.configure(command=self.shutdown_logout)
+        return
+
+    def connect(self):
+        # global client
+        ip = self.f1.input.get()
+        try:
+            self.client.connect((ip, 5656))
+            tk.messagebox.showinfo(message="Connect successfully!")
+            self.show_main_ui()
+        except:
+            tk.messagebox.showerror(message="Cannot connect!")
+        return
 
 
-def live_screen():
-    client.sendall(bytes("LIVESCREEN", "utf8"))
-    tmp = lsc.Desktop_UI(root, client)
-    if tmp.status == False:
-        back(tmp)
-    return
+def main():
+    Client().start()
 
 
-def shutdown_logout():
-    client.sendall(bytes("SD_LO", "utf8"))
-    sl.shutdown_logout(client, root)
-    return
-
-
-def mac_address():
-    client.sendall(bytes("MAC", "utf8"))
-    mac.mac_address(client)
-    return
-
-
-def back_dirTree(ui):
-    ui.place_forget()
-    ui.tree.pack_forget()
-    f2.place(x=0, y=0)
-    client.sendall(bytes("QUIT", "utf8"))
-
-
-def directory_tree():
-    client.sendall(bytes("DIRECTORY", "utf8"))
-    tmp = dt.DirectoryTree_UI(root, client)
-    tmp.button_6.configure(command=lambda: back_dirTree(tmp))
-    return
-
-
-def app_process():
-    client.sendall(bytes("APP_PRO", "utf8"))
-    tmp = ap.App_Process_UI(root, client)
-    tmp.button_6.configure(command=lambda: back(tmp))
-    return
-
-
-def disconnect():
-    f2.place_forget()
-    f1.place(x=0, y=0)
-    client.sendall(bytes("QUIT", "utf8"))
-    return
-
-
-def keylogger():
-    client.sendall(bytes("KEYLOG", "utf8"))
-    tmp = kl.Keylogger_UI(root, client)
-    tmp.button_6.configure(command=lambda: back(tmp))
-    return
-
-
-def registry():
-    client.sendall(bytes("REGISTRY", "utf8"))
-    tmp = rc.Registry_UI(root, client)
-    tmp.btn_back.configure(command=lambda: back_reg(tmp))
-    return
-
-
-def back_reg(ui):
-    ui.client.sendall(bytes("STOP_EDIT_REGISTRY", "utf8"))
-    ui.place_forget()
-    f2.place(x=0, y=0)
-
-
-def show_main_ui():
-    f1.place_forget()
-    global f2
-    f2 = ui2.Main_UI(root)
-    f2.button_1.configure(command=live_screen)
-    f2.button_2.configure(command=registry)
-    f2.button_3.configure(command=mac_address)
-    f2.button_4.configure(command=directory_tree)
-    f2.button_5.configure(command=app_process)
-    f2.button_6.configure(command=disconnect)
-    f2.button_7.configure(command=keylogger)
-    f2.button_8.configure(command=shutdown_logout)
-    return
-
-
-def connect():
-    global client
-    ip = f1.input.get()
-    try:
-        client.connect((ip, 5656))
-        tk.messagebox.showinfo(message="Connect successfully!")
-        show_main_ui()
-    except:
-        tk.messagebox.showerror(message="Cannot connect!")
-    return
-
-
-f1.button_1.configure(command=connect)
-root.mainloop()
+if __name__ == "__main__":
+    main()
