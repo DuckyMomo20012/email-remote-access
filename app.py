6109d1afb15e7ad45d4906de0426c4f89005d1e1
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-07 22:53:04 +0700

    fix: update service invocation

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from enum import Enum
 import typer
 from environs import Env
 
+import src.client.client as client
+import src.server.server as server
+
 TService = Enum("Service", {k: k for k in ["server", "client"]})  # type: ignore
 
 env = Env()
@@ -12,9 +15,9 @@ env.read_env()
 
 def main(service: TService = typer.Argument("server", help="Service to run")):
     if str(service) == "Service.server":
-        exec(open("src/server/server.py").read())
+        server.main()
     elif str(service) == "Service.client":
-        exec(open("src/client/client.py").read())
+        client.main()
 
 
 if __name__ == "__main__":
