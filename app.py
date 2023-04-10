07d0a63e55d8a8fbaf04542494132b6f2914169c
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-10 20:59:32 +0700

    refactor: split server.py to app and server files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import typer
 from environs import Env
 
 import src.client.client as client
-import src.server.server as server
+import src.server.app as serverApp
 
 TService = Enum("Service", {k: k for k in ["server", "client"]})  # type: ignore
 
@@ -15,7 +15,7 @@ env.read_env()
 
 def main(service: TService = typer.Argument("server", help="Service to run")):
     if str(service) == "Service.server":
-        server.main()
+        serverApp.main()
     elif str(service) == "Service.client":
         client.main()
 
--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,3 @@
-import threading
-import tkinter as tk
-
 import socketio
 import uvicorn
 from fastapi import FastAPI
@@ -16,104 +13,63 @@ import src.server.shutdown_logout_server as sl
 BUFSIZ = 1024 * 4
 PORT = 5656
 
+app = FastAPI()
 
-class ServerApp:
-    def __init__(self):
-        self.main = tk.Tk()
-        self.main.geometry("200x200")
-        self.main.title("Server")
-        self.main["bg"] = "plum1"
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*", logger=True)
 
-        self.start_btn = tk.Button(
-            self.main,
-            text="OPEN",
-            width=10,
-            height=2,
-            fg="white",
-            bg="IndianRed3",
-            borderwidth=0,
-            highlightthickness=0,
-            command=self.connect,
-            relief="flat",
-        )
-        self.start_btn.place(x=100, y=100, anchor="center")
-
-    def start(self):
-        print("Server is running...")
-        self.main.mainloop()
-
-    def connect(self):
-        server = Server()
-
-        t = threading.Thread(target=server.start_server)
-        t.daemon = True
-        t.start()
-
-        self.start_btn["state"] = tk.DISABLED
+app = socketio.ASGIApp(sio)
 
 
-class Server:
-    def __init__(self):
-        self.app = FastAPI()
-
-        self.sio = socketio.AsyncServer(
-            async_mode="asgi", cors_allowed_origins="*", logger=True
-        )
-        self.app = socketio.ASGIApp(self.sio)
-
-        # NOTE: Register the callbacks
-        self.callbacks()
-
-    def start_server(self):
-        uvicorn.run(self.app, host="0.0.0.0", port=PORT)
-
-    def callbacks(self):
-        @self.sio.on("KEYLOG:start")
-        def keylogger(sid):
-            # kl.keylog(self.sio)
-            return
-
-        @self.sio.on("SD_LO:start")
-        def shutdown_logout(sid):
-            sl.shutdown_logout(self.sio)
-            return
-
-        @self.sio.on("MAC:start")
-        async def mac_address(sid):
-            await mac.mac_address(self.sio)
-            return
-
-        @self.sio.on("APP_PRO:start")
-        def app_process(sid):
-            ap.app_process(self.sio)
-            return
-
-        @self.sio.on("LIVESCREEN:start")
-        async def live_screen(sid):
-            await lss.capture_screen(self.sio)
-            return
-
-        @self.sio.on("DIRECTORY:start")
-        def directory_tree(sid):
-            dt.directory(self.sio)
-            return
-
-        @self.sio.on("REGISTRY:start")
-        def registry(sid):
-            rs.registry(self.sio)
-            return
-
-        @self.sio.on("QUIT")
-        def quit(sid):
-            exit(1)
-            return
+@sio.on("KEYLOG:start")
+def keylogger(sid):
+    # kl.keylog(sio)
+    return
 
 
-###############################################################################
+@sio.on("SD_LO:start")
+def shutdown_logout(sid):
+    sl.shutdown_logout(sio)
+    return
+
+
+@sio.on("MAC:start")
+async def mac_address(sid):
+    await mac.mac_address(sio)
+    return
+
+
+@sio.on("APP_PRO:start")
+def app_process(sid):
+    ap.app_process(sio)
+    return
+
+
+@sio.on("LIVESCREEN:start")
+async def live_screen(sid):
+    await lss.capture_screen(sio)
+    return
+
+
+@sio.on("DIRECTORY:start")
+def directory_tree(sid):
+    dt.directory(sio)
+    return
+
+
+@sio.on("REGISTRY:start")
+def registry(sid):
+    rs.registry(sio)
+    return
+
+
+@sio.on("QUIT")
+def quit(sid):
+    exit(1)
+    return
 
 
 def main():
-    ServerApp().start()
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
 
 
 if __name__ == "__main__":
