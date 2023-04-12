092d12d88ea6525822507c038e92d4484e8f2fae
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-12 19:59:18 +0700

    refactor(server, client): update shutdown logout socketio event

--- src/server/shutdown_logout_server.py
+++ src/server/shutdown_logout_server.py
@@ -5,13 +5,19 @@ import socketio
 
 def callbacks(sio: socketio.AsyncServer):
     @sio.on("SD_LO:shutdown")
-    def on_shutdown(sid):
-        print("shutdown")
-        # FIXME: Uncomment this line
-        # os.system("shutdown -s -t 15")
+    def on_shutdown(sid, data):
+        try:
+            print("shutdown")
+            # FIXME: Uncomment this line
+            # os.system("shutdown -s -t 15")
+        except Exception:
+            return {"msg": "Error while shutting down"}
 
     @sio.on("SD_LO:logout")
-    def on_logout(sid):
-        print("logout")
-        # FIXME: Uncomment this line
-        # os.system("shutdown -l")
+    def on_logout(sid, data):
+        try:
+            print("logout")
+            # FIXME: Uncomment this line
+            # os.system("shutdown -l")
+        except Exception:
+            return {"msg": "Error while logging out"}
