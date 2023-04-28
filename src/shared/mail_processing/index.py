525050e0406b26b6186c4b657b9a7f0cce622f1b
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-04-28 09:20:57 +0700

    chore(shared): update cmd mapping

--- src/shared/mail_processing/index.py
+++ src/shared/mail_processing/index.py
@@ -18,31 +18,26 @@ from src.shared.mail_processing.shutdown_logout import (
 )
 from src.shared.mail_processing.utils import Command
 
+cmdMapping = {
+    "shutdown": onShutdownMessage,
+    "logout": onLogoutMessage,
+    "mac_address": onMacAddressMessage,
+    "screenshot": onScreenshotMessage,
+    "list_directory": onListDirectoryMessage,
+    "copy_file_to_server": onCopyFileToServerMessage,
+    "copy_file_to_client": onCopyFileToClientMessage,
+    "delete_file": onDeleteFileMessage,
+    "list_process": onListProcessMessage,
+    "list_application": onListApplicationMessage,
+    "kill_process": onKillProcessMessage,
+}
+
 
 def runCmd(service, sio, cmd: Command, reqMessage, reply=True):
     try:
-        if cmd["type"] == "shutdown":
-            onShutdownMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "logout":
-            onLogoutMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "mac_address":
-            onMacAddressMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "screenshot":
-            onScreenshotMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "list_directory":
-            onListDirectoryMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "copy_file_to_server":
-            onCopyFileToServerMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "copy_file_to_client":
-            onCopyFileToClientMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "delete_file":
-            onDeleteFileMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "list_process":
-            onListProcessMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "list_application":
-            onListApplicationMessage(service, sio, cmd, reqMessage, reply=reply)
-        elif cmd["type"] == "kill_process":
-            onKillProcessMessage(service, sio, cmd, reqMessage, reply=reply)
+        if cmd["type"] in cmdMapping:
+            cmdHandler = cmdMapping[cmd["type"]]
+            cmdHandler(service, sio, cmd, reqMessage, reply)
         else:
             return False
 
