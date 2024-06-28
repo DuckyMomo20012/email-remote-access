3995b7fad9c9ba8ae793894c9d5f9b69530a27f7
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-06-28 12:22:52 +0700

    fix(newClientApp): dynamically switch windows

--- src/newClientApp/pages/index/index.py
+++ src/newClientApp/pages/index/index.py
@@ -5,10 +5,37 @@ import dearpygui.dearpygui as dpg
 from src.newClientApp.pages.index.list_process import ListProcessWindow
 from src.shared.pages.base import BasePage
 
+windows: dict[str, BasePage] = {
+    "/proc": ListProcessWindow,
+}
+
 
 class IndexPage(BasePage):
     def __init__(self, tag: Union[int, str] = "w_index"):
         super().__init__(tag)
+        self.prevWindow: Union[int, str] = ""
+        self.prevButton: Union[int, str] = ""
+
+    def assignWindow(self, route: str, parent: Union[int, str], sender):
+        try:
+            w = windows.get(route)
+
+            if w is None:
+                raise ValueError(f"Route {route} is not found")
+
+            w = w(parent=parent)
+
+            if self.prevWindow != "" and self.prevButton != "":
+                dpg.configure_item(self.prevButton, enabled=True)
+                dpg.delete_item(self.prevWindow)
+            self.prevWindow = w.tag
+            self.prevButton = sender
+            dpg.configure_item(self.prevButton, enabled=False)
+
+            w.render()
+
+        except ValueError:
+            print("ValueError: Route is not found")
 
     def render(self):
         with dpg.window(label="Home", tag=self.tag, width=400, height=200):
@@ -20,14 +47,13 @@ class IndexPage(BasePage):
 
             g_body = dpg.add_group(horizontal=True)
 
-            # NOTE: Have to render within context, and MUST pass a parent to it
-            def renderListProcess():
-                ListProcessWindow(parent=w).render()
-
             with dpg.group(width=200, parent=g_body):
                 dpg.add_button(
                     label="List process",
-                    callback=renderListProcess,
+                    # NOTE: Have to render within context, and MUST pass a parent to it
+                    callback=lambda sender: self.assignWindow(
+                        "/proc", parent=w, sender=sender
+                    ),
                 )
                 dpg.add_button(label="List directory")
                 dpg.add_button(label="Registry")
